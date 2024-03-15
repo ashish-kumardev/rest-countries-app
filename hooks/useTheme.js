44bd6007936d69fe68a48c9305ccbae192b9e4db
Ashish-Abhiwan <ashish.abhiwan@gmail.com>
2024-03-15 16:47:49 +0530

    rm logs of error

--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
 
 export function useTheme() {
-  const [isDark, setIsDark] = useContext(ThemeContext)
-  console.log(isDark, typeof setIsDark)
-  return [isDark, setIsDark]
+  return ([isDark, setIsDark] = useContext(ThemeContext));
+  // return [isDark, setIsDark]
+  // console.log(isDark, typeof setIsDark)
 }
