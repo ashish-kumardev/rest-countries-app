d37447a0ee8d96b6f3b1afcbeac937f80262d838
Ashish-Abhiwan <ashish.abhiwan@gmail.com>
2024-03-15 16:53:23 +0530

    fix : isDark not defined error v1

--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
 
 export function useTheme() {
-  return ([isDark, setIsDark] = useContext(ThemeContext));
-  // return [isDark, setIsDark]
+  const [isDark, setIsDark] = useContext(ThemeContext);
+  return [isDark, setIsDark]
   // console.log(isDark, typeof setIsDark)
 }
